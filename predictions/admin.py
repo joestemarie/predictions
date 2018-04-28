e9e6fd2ec4620375f2313c848d99b676fadc3108
Joe Ste.Marie <jmarie@bpimedia.com>
2018-04-28 13:01:25 -0400

    Working user profile, closes #3

--- predictions/admin.py
+++ predictions/admin.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Prediction, PredictionTag
+from .models import Prediction, PredictionTag, Profile
 
 # Register your models here.
 admin.site.register(Prediction)
 admin.site.register(PredictionTag)
+admin.site.register(Profile)
